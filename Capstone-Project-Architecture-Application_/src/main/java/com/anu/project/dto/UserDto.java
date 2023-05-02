fbe05587f18cd6b7caf2872841b6df625bcc89b1
Anup7676 <anupallie@gmail.com>
2023-05-02 16:59:45 +0530

    safe commit

---
+++ Capstone-Project-Architecture-Application_/src/main/java/com/anu/project/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.anu.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    
+}
